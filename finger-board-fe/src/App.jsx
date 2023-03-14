499b5e19af98faef2cb5d920413e8f70987cdb77
dci-student <thanhhoa158169@gmail.com>
2023-03-14 13:02:50 +0100

    added  Form

---
+++ finger-board-fe/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Form from './components/Form'
+import './App.css'
+
+
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <header></header>
+      <h1>Finger Board</h1>
+      <Form></Form>
+    </div>
+  )
+}
+
+export default App
