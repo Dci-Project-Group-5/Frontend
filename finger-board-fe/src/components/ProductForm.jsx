0d922531777c2ed8e7b64979c883fc314c494d9f
HannesHerbert <hannespetri89@gmail.com>
2023-03-16 11:12:24 +0100

    ProductForm bearbeitet

--- finger-board-fe/src/components/ProductForm.jsx
+++ finger-board-fe/src/components/ProductForm.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useState } from "react";
-// import axios from "axios";
-// import defaultImage from "/img/81m1chz4kQL._AC_SL1500_.jpg"
+import axios from "axios";
+import defaultFrontImage from "../../img/stars_of_oilspill-back.png";
+import defaultBackImage from "../../img/stars_of_oilspill-front.png";
 import { Link } from 'react-router-dom';
 
 function ProductForm() {
 
-
-    // const [frontImage, setFrontImage] = useState(defaultImage);
-    // const [frontImage, setFrontImage] = useState(null);
-
+    const [productName, setProductName] = useState('');
+    const [frontImage, setFrontImage] = useState(defaultFrontImage);
+    const [backImage, setBackImage] = useState(defaultBackImage);
+    const [wood, setWood] = useState('');
+    const [color, setColor] = useState('');
+    const [description, setDescription] = useState('');
+    const [size, setSize] = useState('');
+    const [price, setPrice] = useState(0);
 
     async function handleSubmit(evt) {
+        
         evt.preventDefault();
-        console.log('product submit');
 
-/* 
-        let body = {frontImage: frontImage};
+        let body = {
+            productname: productName,
+            image: {
+                front: frontImage,
+                back: backImage
+            },
+            wood: wood,
+            color: color,
+            description: description,
+            size: size,
+            price: price
+        };
+
+        console.log(body);
     
             try {
-                let resp = await axios.post('http://localhost:6000/api/v1/product/saveProduct', {frontImage: frontImage}, {
+                let resp = await axios.post('http://localhost:8080/api/v1/product/saveProduct', body, {
                     headers: {"Content-Type" : "application/json"}
                     // withCredentials: true
                 });
@@ -33,12 +50,14 @@ function ProductForm() {
                 // if (error.response.status === 403) setNotVerified(true);
                 // setErrors([error.response.data.message]);
             }
-         */
+        
     }
 
     function setImage(evt) {
 
-        console.log('file change');
+        const inputId = evt.target.id
+
+        console.log(inputId);
 
         const file = evt.target.files[0];
 
@@ -48,13 +67,13 @@ function ProductForm() {
 
         fileReader.onloadend = (evt) => {
             const fileData = fileReader.result;
-            setFrontImage(fileData)
+
+            //Je nach Input (evt.target) wird Front- oder Backimage gesetet
+            inputId === "formFile1" ? setFrontImage(fileData) : setBackImage(fileData);
         }
 
         
     }
-
-    // console.log(frontImage);
 
     return (
         <div id='addProduct' className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -78,6 +97,7 @@ function ProductForm() {
                                 required
                                 className="relative block mb-3 w-full rounded-md border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder="Produktname"
+                                onChange={(evt) => setProductName(evt.target.value)}
                             />
                         </div>
 
@@ -93,7 +113,6 @@ function ProductForm() {
                                     type="file"
                                     accept="image/*"
                                     id="formFile1"
-                                    // value={frontImage}
                                     onChange={setImage}
                                     />
                             </div>
@@ -109,7 +128,10 @@ function ProductForm() {
                                 <input
                                     className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 dark:border-neutral-600 bg-clip-padding py-[0.32rem] px-3 text-base font-normal text-neutral-700 dark:text-neutral-200 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 dark:file:bg-neutral-700 file:px-3 file:py-[0.32rem] file:text-neutral-700 dark:file:text-neutral-100 file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] file:[border-inline-end-width:1px] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-[0_0_0_1px] focus:shadow-primary focus:outline-none"
                                     type="file"
-                                    id="formFile2" />
+                                    accept="image/*"
+                                    id="formFile2"
+                                    onChange={setImage}
+                                    />
                             </div>
                         </div>
 
@@ -121,6 +143,7 @@ function ProductForm() {
                                 required
                                 className="relative mb-3 block w-full rounded border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder="Produktbeschreibung"
+                                onChange={(evt) => setDescription(evt.target.value)}
                             />
                         </div>
 
@@ -132,6 +155,7 @@ function ProductForm() {
                                 required
                                 className="relative mb-3 block w-full rounded border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder="Holzart"
+                                onChange={(evt) => setWood(evt.target.value)}
                             />
                         </div>
 
@@ -143,6 +167,7 @@ function ProductForm() {
                                 required
                                 className="relative mb-3 block w-full rounded border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder="Farbe"
+                                onChange={(evt) => setColor(evt.target.value)}
                             />
                         </div>
 
@@ -154,6 +179,7 @@ function ProductForm() {
                                 required
                                 className="relative mb-3 block w-full rounded border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder="Maße in mm"
+                                onChange={(evt) => setSize(evt.target.value)}
                             />
                         </div>
 
@@ -165,6 +191,7 @@ function ProductForm() {
                                 required
                                 className="relative mb-3 block w-full rounded border-0 p-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder="Preis in €"
+                                onChange={(evt) => setPrice(evt.target.value)}
                             />
                         </div>
 
@@ -175,7 +202,9 @@ function ProductForm() {
                             type="submit"
                             className="relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
-                           <Link to= '/produkt'> <h3 className="h3">Hinzufügen</h3></Link>
+                           {/* <Link to= '/produkt'> <h3 className="h3"> */}
+                            Hinzufügen
+                            {/* </h3></Link> */}
                         </button>
                     </div>
                 </form>
