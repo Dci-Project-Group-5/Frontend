4fbf6467b5f3ea01933121474d29226c6b6f54ca
HannesHerbert <hannespetri89@gmail.com>
2023-03-17 14:30:14 +0100

    UserUpdateForm added

--- finger-board-fe/src/components/Home.jsx
+++ finger-board-fe/src/components/Home.jsx
@@ -11,6 +11,7 @@ function Home() {
   const logout = async () => {
 
     const uri = "https://finger-board.onrender.com/api/v1/user/logout";
+    //const uri = "http://localhost:8080/api/v1/user/logout";
 
     try {
       await axios.get(uri);
