af38a54b9befb9b22ea171a9650b60f6e50ed62c
HannesHerbert <hannespetri89@gmail.com>
2023-03-17 23:18:04 +0100

    modified:   finger-board-fe/src/components/Dashboard.jsx 	new file:   package-lock.json

--- finger-board-fe/src/components/Dashboard.jsx
+++ finger-board-fe/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState,useContext } from "react";
 import { Navigate, NavLink } from "react-router-dom";
 import UserContext from "../context/UserContext.jsx";
 import UserEditForm from "./UserEditForm.jsx";
@@ -12,9 +12,9 @@ function Dashboard() {
     return <Navigate to="/" />;
   }
   const { username, email, password, _id } = state.user;
+  
 
-
-  // HOA: diese Funktion soll path auf UserEditForm öffnen
+// HOA: diese Funktion soll path auf UserEditForm öffnen
   function handleEditBtn() {
     
   }
@@ -23,96 +23,39 @@ function Dashboard() {
   return (
 
     <div className="dashboard-container">
+        <div className="link-db">
+          <NavLink to="/">
+            <h1>Dashboard</h1>{" "}
+          </NavLink>
+        </div>
 
-      <div className="link-db">
-        <NavLink to="/">
-          <h1>Dashboard</h1>{" "}
-        </NavLink>
+        <div className="dh-cards">
 
+          <div className="relative bg-white">
+            <Link to='/editform'><button className="absolute right-0">Daten bearbeiten</button></Link>
+            
+            <h3 className="user-db font-bold text-purple-800">USER DATEN</h3>
+            <div className="card-db ">
+              <p className="font-bold mb-2 text-2xl text-purple-800">Username : {username} </p>
+              <p className="font-bold mb-2 text-2xl text-purple-800">Email : {email} </p>
+              <p className="font-bold mb-2 text-2xl text-purple-800">Password : ****** </p>
+              <NavLink to="/produkt" className="shop-more text-purple-600 hover:text-purple-500 underline text-sm">Shoping More 👉 </NavLink>
+            </div> 
+           
 
-      </div>
-
-
-      <div className="dh-cards  ">
-
-        <div className="relative bg-white">
-
-          <button onClick={handleEditBtn} className="absolute right-0">Daten bearbeiten</button>
-
-          <p className="user-db"> Hi , {username} welcome to the Dashboard!</p>
-          <h3 className="user-db font-bold text-purple-800">USER DATEN</h3>
-          <div className="card-db ">
-            <p className="font-bold mb-2 text-2xl text-purple-800">Username : {username} </p>
-            <p className="font-bold mb-2 text-2xl text-purple-800">Email : {email} </p>
-            <p className="font-bold mb-2 text-2xl text-purple-800">Password : ****** </p>
-
-
-
-            {/* <a href="#" className="shop-more text-purple-600 hover:text-purple-500 underline text-sm"></a> */}
-
-            <div className=" bg-white">
-              <p className="user-db"> Hi , {username} welcome to the Dashboard!</p>
-              <h3 className="user-db font-bold text-purple-800">USER DATEN</h3>
-              <div className="card-db ">
-                <p className="font-bold mb-2 text-2xl text-purple-800">Username : {username} <button>edit</button></p>
-                <p className="font-bold mb-2 text-2xl text-purple-800">Email : {email} <button>edit</button></p>
-                <p className="font-bold mb-2 text-2xl text-purple-800">Password : ****** <button>edit</button></p>
-                <NavLink to="/produkt" className="shop-more text-purple-600 hover:text-purple-500 underline text-sm">Shoping More 👉 </NavLink>
-                <a href="#" className="shop-more text-purple-600 hover:text-purple-500 underline text-sm">
-
-                  <NavLink to="/produkt">
-                    Shoping More 👉
-                  </NavLink>
-                </a>
-              </div>
-
-            </div>
-
-            <div className="link-db">
-              <NavLink to="/">
-                <h1>Dashboard</h1>{" "}
-              </NavLink>
-            </div>
-
-            <div className="dh-cards">
-
-              <div className="relative bg-white">
-                <Link to='/editform'><button className="absolute right-0">Daten bearbeiten</button></Link>
-
-                <h3 className="user-db font-bold text-purple-800">USER DATEN</h3>
-                <div className="card-db ">
-                  <p className="font-bold mb-2 text-2xl text-purple-800">Username : {username} </p>
-                  <p className="font-bold mb-2 text-2xl text-purple-800">Email : {email} </p>
-                  <p className="font-bold mb-2 text-2xl text-purple-800">Password : ****** </p>
-                  <NavLink to="/produkt" className="shop-more text-purple-600 hover:text-purple-500 underline text-sm">Shoping More 👉 </NavLink>
-                </div>
-
-
-
-              </div>
-              <div className="member">
-                <h4>BESTELLUNGEN</h4>
-                <div>Du hast noch keine Bestellung!</div>
-
-              </div>
-
-            </div>
-
-
-            <div className="member">
+          </div>
+          <div className="member">
               <h4>BESTELLUNGEN</h4>
               <div>Du hast noch keine Bestellung!</div>
 
-
             </div>
-          </div >
-        </div >
-      </div>
 
 
+        </div>
 
-    </div >
 
+    </div>
+    
   );
 }
 
